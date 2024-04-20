19161f80d2b018b1c258d8c49e19b49bbed04b53
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-04-20 13:18:31 +0530

    updated styles

--- script.js
+++ script.js
@@ -14,48 +14,42 @@ tl.from('#texter h1',{
     delay:0.2,
     stagger:0.2
 })
-tl.to('#img1',{
+gsap.to('#img1',{
     x:-500,
     duration:1,
-    opacity:0,
+    // opacity:0,
     delay:0.2,
 })
-tl.to('#img2',{
+gsap.to('#img2',{
     x:500,
     duration:1,
-    opacity:0,
+    // opacity:0,
     delay:0.2,
 })
-tl.to('#img3',{
-    y:200,
+gsap.to('#img3',{
+    y:-60,
     duration:1,
-    opacity:0,
+    // opacity:0,
     delay:0.2,
 })
 
-tl.to('#img2',{
-    rotate:9,
+gsap.to('#img2',{
+    rotate:30,
     delay:2,
-    x:150,
+    x:170,
     duration:1,
 })
-tl.to('#img1',{
-    rotate:-10,
+gsap.to('#img1',{
+    rotate:-30,
     delay:2,
-    x:-150,
+    x:-170,
     duration:1,
 })
-tl.to('#img3',{
+gsap.to('#img3',{
     // opacity:0,
-    x:150,
+    // x:120,
     duration:0.5,
     delay:0.2,
     stagger:0.2,
     // rotate:250,
-})
-gsap.to('h1',{
-    y:-150,
-    duration:2,
-    delay:4,
-    // stagger:0.2
 })
